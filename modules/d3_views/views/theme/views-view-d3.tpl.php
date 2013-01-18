d281f1cd0cdf79ca4f7021a2bdd13ce42d21ff6e
Alan Sherry <alan.sherry@freeflowdigital.com>
2013-01-17 17:11:48 -0800

    Changed: improved support for views

--- modules/d3_views/views/theme/views-view-d3.tpl.php
+++ modules/d3_views/views/theme/views-view-d3.tpl.php
@@ -3,30 +3,27 @@
 /**
  * @file
  * Template to display a view as a table.
- *
- * - $title : The title of this group of rows.  May be empty.
+ * 
+ * - $visualization: The rendered chart html. No JS.
  * - $header: An array of header labels keyed by field id.
- * - $header_classes: An array of header classes keyed by field id.
- * - $fields: An array of CSS IDs to use for each field id.
- * - $classes: A class or classes to apply to the table, based on settings.
- * - $row_classes: An array of classes to apply to each row, indexed by row
- *   number. This matches the index in $rows.
  * - $rows: An array of row items. Each row is an array of content.
- *   $rows are keyed by row number, fields within rows are keyed by field ID.
- * - $field_classes: An array of classes to apply to each field, indexed by
- *   field id, then row number. This matches the index in $rows.
+ *   $rows are keyed by row number, fields within rows are keyed numerically 
+ *   for the purposes of chart rendering.
+ * - $show_table: A boolean indicating whether or not to show the debug
+ *   table.
  * @ingroup views_templates
  */
 ?>
-<table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
-  <?php if (!empty($title)) : ?>
-    <caption><?php print $title; ?></caption>
-  <?php endif; ?>
+<?php print $visualization; ?>
+
+<?php if ($show_table): ?>
+
+<table id="views-debug-table">
   <?php if (!empty($header)) : ?>
     <thead>
       <tr>
         <?php foreach ($header as $field => $label): ?>
-          <th <?php if ($header_classes[$field]) { print 'class="'. $header_classes[$field] . '" '; } ?>>
+          <th>
             <?php print $label; ?>
           </th>
         <?php endforeach; ?>
@@ -35,9 +32,9 @@
   <?php endif; ?>
   <tbody>
     <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
+      <tr>
         <?php foreach ($row as $field => $content): ?>
-          <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?>>
+          <td>
             <?php print $content; ?>
           </td>
         <?php endforeach; ?>
@@ -45,3 +42,5 @@
     <?php endforeach; ?>
   </tbody>
 </table>
+
+<?php endif; ?>
