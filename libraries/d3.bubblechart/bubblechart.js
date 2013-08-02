8fcf796e13be093f7b3de2860b8261b3cda4b5e0
Joseph Ssekono <joseph@mountbatten.net>
2013-08-02 17:45:07 +0300

    D3 bubble chart library added

--- libraries/d3.bubblechart/bubblechart.js
+++ libraries/d3.bubblechart/bubblechart.js
@@ -23,14 +23,14 @@
     //Setting dimension of the draw area occupied by chart in pixels.
     diameter = settings.width,
     format = d3.format(",d"),
-    color = d3.scale.category20c();
+    color = d3.scale.ordinal().range(settings.series_colours);
 
     var bubble = d3.layout.pack()
         .sort(null)
         .size([diameter, diameter])
         .padding(1.5);
 
-    var svg = d3.select("#visualization").append("svg")
+    var svg = d3.select("#"+settings.id).append("svg")
         .attr("width", diameter)
         .attr("height", diameter)
         .attr("class", "bubble")
@@ -42,14 +42,21 @@
           .filter(function(d) { return !d.children; }))
           .enter().append("g")
           .attr("class", "node")
+          .attr("id", function(d, i) { return "node-"+i; })
           .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-      node.append("title")
-          .text(function(d) { return d.className + ": " + format(d.value); });
+//      node.append("title")
+//          .text(function(d) { return d.className + ": " + format(d.value); });
 
       node.append("circle")
           .attr("r", function(d) { return d.r; })
-          .style("fill", function(d) { return color(d.packageName); });
+          .style("fill", function(d, i) { return color(i); })
+          .attr('stroke', function(d, i) { return color(i); })
+          .attr('stroke-width', '1')
+          .attr('opacity', '1')
+          .on('mouseover', function(d, i) { interact('over', i); })
+          .on('mouseout', function(d, i) { interact('out', i); })
+          .attr('class', function(d, i) { return 'color_' + (color(i)).replace("#","") + ' bubble-' + i; });
 
       node.append("text")
           .attr("dy", ".3em")
@@ -71,8 +78,80 @@
       return {children: classes};
     }
 
-    d3.select(self.frameElement).style("height", diameter + "px");
+    /**
+     * Wrapper function for all rollover functions.
+     *
+     * @param string text
+     *   Current state, 'over', or 'out'.
+     * @param int i
+     *   Current index of the current data row.
+     * @return none
+     */
+    function interact(state, i) {
+      if (state == 'over') {
+        highlightBubble(i);
+        showTooltip(i);
+      }
+      else {
+        unhighlightBubble(i);
+        hideTooltip(i);
+      }
+      return true;
+    }
 
+    //mouse over and out effects
+    function highlightBubble(i) {
+        d3.selectAll(".bubble-" + i)
+        .attr('stroke', '#ccc')
+        .attr('stroke-width', '1')
+        .attr('opacity', '0.75');
+      }
+
+    function unhighlightBubble(i) {
+        d3.selectAll(".bubble-" + i)
+        .attr('stroke', color(i))
+        .attr('stroke-width', '1')
+        .attr('opacity', '1');
+      }
+
+    function showTooltip(i) {
+        var tip = d3.selectAll("#node-"+i).append("g")
+        .attr("id", function(d, i) { return "tooltip-"+i; })
+        .attr("class", "tooltip")
+        .style("z-index", 10000)
+        .attr("transform", "translate("+-50+","+-50+")");
+
+        tip.append("rect")
+        .attr("class", "tooltip")
+        .attr("width", "115")
+        .attr("height", 40)
+        .attr("fill", "white")
+        .style("stroke", "#cccccc");
+
+        var tiptext = tip.append("g")
+        .attr("class", "tooltip")
+        .attr("font-size","10px")
+        .style("font-family", "sans-serif");
+
+        tiptext.append("text")
+        .attr("id", "title-"+i)
+        .attr("dx", "0.5em")
+        .attr("dy", "1.5em")
+        .text(function(d) { return d.className; });
+
+        tiptext.append("text")
+        .attr("id", "value-"+i)
+        .attr("dx", "0.5em")
+        .attr("dy", "3em")
+        .text(function(d) { return "Value: " + format(d.value); });
+
+    }
+
+    function hideTooltip(i) {
+        d3.selectAll("#node-"+i).select("g.tooltip").remove();
+    }
+
+    d3.select(self.frameElement).style("height", diameter + "px");
   };
 
 })(jQuery);
