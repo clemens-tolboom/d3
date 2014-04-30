56b94df9f925fd3a53516ee5514bdb6261a12142
markcarver <markcarver@501638.no-reply.drupal.org>
2014-04-30 13:43:15 -0700

    Issue #2255069 by Mark Carver | drumm: Only run attach behavior once (and general cleanup).

--- js/d3.js
+++ js/d3.js
@@ -1,43 +1,39 @@
 /**
  * @file
- * Javascript file that initiates the d3 and behaviors globals.
+ * Integrates d3 library and functionality with D7 core javascript
  */
 
-/**
- * Integrates D3 library and functionality with D7 core javascript
- */
-(function($) {
+(function($, Drupal) {
+
+  /**
+   * Global object for holding d3 internal and visualization style methods.
+   */
   Drupal.d3 = {
+    draw: function(element, settings) {
+      // Invoke JavaScript library function, if it exists.
+      if (Drupal.d3[settings.type]) {
+        Drupal.d3[settings.type](element, settings);
+      }
+    }
+  };
 
-    draw : function(element, settings) {
-      // type of chart
-      var vType = settings.type;
-
-      // invoke javascript library function
-      Drupal.d3[vType](element, settings);
-    },
-
-  }
-})(jQuery);
-
-(function($) {
-Drupal.behaviors.d3 = {
-  attach: function(context, settings) {
-    // check to see if there are visualizations that have been set
-    if (settings.d3.inventory) {
-      var vis = new Object;
-
-      // for each of the visualizations set in inventory
-      for (var visId in settings.d3.inventory) {
-        // if the container for this visualization exists on the page
-        if (jQuery('#' + visId, context).length) {
-          Drupal.d3.draw(visId, settings.d3.inventory[visId]);
+  /**
+   * Drupal behaviors for D3 module.
+   */
+  Drupal.behaviors.d3 = {
+    attach: function(context, settings) {
+      var id, $context = $(context);
+      // Check to see if there are visualizations that have been set.
+      if (settings.d3 && settings.d3.inventory) {
+        // Iterate over each of the visualizations set in inventory.
+        for (id in settings.d3.inventory) {
+          // Only process the visualization once, if it exists.
+          $context.find('#' + id).once('d3', function () {
+            Drupal.d3.draw(id, settings.d3.inventory[id]);
+          });
         }
       }
     }
-  }
-}
-})(jQuery);
+  };
 
-// initialize global object
-Drupal.settings.d3 = (Drupal.settings.d3 | []);
+})(jQuery, Drupal);
